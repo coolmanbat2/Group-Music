5d8163b5d3d06a63c45b766010e78e9e2a15ea02
Thanusun Thayaparan <thanusun.thayaparan@mail.utoronto.ca>
2022-08-14 08:21:53 -0400

    Updated createRoomPage to be reusable for updates

--- frontend/src/components/createRoomPage.js
+++ frontend/src/components/createRoomPage.js
@@ -9,44 +9,120 @@ import {
   Radio,
   RadioGroup,
   FormControlLabel,
+  Collapse,
+  Alert,
 } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import GetCookie from "./GetCookie";
 
 function CreateRoomPage(props) {
-  const defaultVotes = 2;
   const navigate = useNavigate();
-  const [guestCanPause, setGuestCanPause] = useState(true);
-  const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
+  const [guestCanPause, setGuestCanPause] = useState(props.guestCanPause);
+  const [voteToSkip, setVoteToSkip] = useState(props.voteToSkip);
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
 
-  const handleVotesChange = (e) => {
-    setVotesToSkip(e.target.value);
-  };
+  function handleVotesChange(e) {
+    setVoteToSkip(e.target.value);
+  }
 
-  const handleGuestCanPauseChange = (e) => {
-    setGuestCanPause(e.target.value === "true" ? true : false);
-  };
+  function handleGuestCanPauseChange(e) {
+    setGuestCanPause(e.target.value === "playpause" ? true : false);
+  }
 
-  const handleRoomButtonPressed = () => {
+  function handleRoomButtonPressed() {
     const csrfToken = GetCookie();
     const requestOptions = {
       method: "POST",
       headers: { "Content-type": "application/json", "X-CSRFToken": csrfToken },
       body: JSON.stringify({
-        votes_to_skip: votesToSkip,
+        votes_to_skip: voteToSkip,
         guest_can_pause: guestCanPause,
       }),
     };
     fetch("/api/create-room", requestOptions)
       .then((response) => response.json())
       .then((data) => navigate("/room/" + data.code));
-  };
+  }
+
+  function handleUpdateButtonPressed() {
+    const csrfToken = GetCookie();
+    const requestOptions = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json", "X-CSRFToken": csrfToken },
+      body: JSON.stringify({
+        votes_to_skip: voteToSkip,
+        guest_can_pause: guestCanPause,
+        code: props.roomCode,
+      }),
+    };
+    fetch("/api/update-room", requestOptions).then((response) => {
+      if (response.ok) {
+        setMessage("Successfully updated room.");
+        setIsError(false);
+      } else {
+        setMessage("Unsucessfully updated room. Error: " + response.statusText);
+        setIsError(true);
+      }
+    });
+  }
+
+  function renderCreateButtons() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleRoomButtonPressed}
+          >
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  function renderUpdateButtons() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleUpdateButtonPressed}
+        >
+          Update Room
+        </Button>
+      </Grid>
+    );
+  }
+
+  const title = props.update ? "Update Room" : "Create a Room";
+  const severity = isError ? "error" : "success";
 
   return (
     <Grid container spacing={1}>
       <Grid item xs={12} align="center">
+        <Collapse in={message != ""}>
+          <Alert
+            onClose={() => {
+              setMessage("");
+            }}
+            variant="filled"
+            severity={severity}
+          >
+            {message}
+          </Alert>
+        </Collapse>
+      </Grid>
+      <Grid item xs={12} align="center">
         <Typography component="h4" variant="h4">
-          Create A Room
+          {title}
         </Typography>
       </Grid>
       <Grid item xs={12} align="center">
@@ -56,7 +132,7 @@ function CreateRoomPage(props) {
           </FormHelperText>
           <RadioGroup
             row
-            defaultValue="playpause"
+            defaultValue={props.guestCanPause ? "playpause" : "nocontrol"}
             onChange={handleGuestCanPauseChange}
           >
             <FormControlLabel
@@ -79,7 +155,7 @@ function CreateRoomPage(props) {
           <TextField
             required={true}
             type="number"
-            defaultValue={defaultVotes}
+            defaultValue={voteToSkip}
             inputProps={{
               min: 1,
               style: { textAlign: "center" },
@@ -91,22 +167,16 @@ function CreateRoomPage(props) {
           </FormHelperText>
         </FormControl>
       </Grid>
-      <Grid item xs={12} align="center">
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={handleRoomButtonPressed}
-        >
-          Create A Room
-        </Button>
-      </Grid>
-      <Grid item xs={12} align="center">
-        <Button color="secondary" variant="contained" to="/" component={Link}>
-          Back
-        </Button>
-      </Grid>
+      {props.update ? renderUpdateButtons() : renderCreateButtons()}
     </Grid>
   );
 }
+
+CreateRoomPage.defaultProps = {
+  update: false,
+  voteToSkip: 2,
+  guestCanPause: true,
+  roomCode: null,
+};
 
 export default CreateRoomPage;
